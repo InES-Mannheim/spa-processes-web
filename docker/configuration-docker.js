15d4d8552db80afd43df5817297e1bf62902dee6
Piero Divasto <pdivasto@mail.uni-mannheim.de>
2016-03-22 19:24:58 +0100

    Removed unnecessary line & file

--- docker/configuration-docker.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-/* REST API Server Configuration*/
-var spaWebConfiguration = angular.module('spaWebConfiguration', [])
-                                 .constant('SERVER_ADDRESS','/api');
