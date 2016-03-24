34f0d16f3fd5f47ee264261dcfbc024b54b62e7c
Piero Divasto <pdivasto@gmail.com>
2016-03-24 15:32:10 +0100

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -4,6 +4,11 @@ rm -rf docker-compose/app
 
 mkdir -p docker-compose/app
 
+# Copy the /app folder which contains the web application into docker-compose folder
 cp -R app/* docker-compose/app/
 
+# Running docker compose using the docker-compose-local.yml configuration file, which will
+# use the local files instead of the images from docker hub. In the case you want to use the 
+# docker hub images of spa-processes-web and spa-processes-rest, please use the the configuration
+# file docker-compose.yml which is in docker-compose folder.
 docker-compose -f docker-compose/docker-compose.yml up -d
